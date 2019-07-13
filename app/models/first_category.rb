779f52be8bf3b95fdc40169fb0f44c80b7f4b15d
Hiroto <heeilxh.sev.012@gmail.com>
2019-07-13 15:53:35 +0900

    Change ActiveHash of Active Hash

---
+++ app/models/first_category.rb
@@ -0,0 +1,19 @@
+class FirstCategory < ActiveHash::Base
+  field :first_category
+  create :id => 1, :first_category => 'レディース'
+  create :id => 2, :first_category => 'メンズ'
+  create :id => 3, :first_category => 'ベビー・キッズ'
+  create :id => 4, :first_category => 'インテリア・住まい・小物'
+  create :id => 5, :first_category => '本・音楽・ゲーム'
+  create :id => 6, :first_category => 'おもちゃ・ホビー・グッズ'
+  create :id => 7, :first_category => 'コスメ・香水・美容'
+  create :id => 8, :first_category => '家電・スマホ・カメラ'
+  create :id => 9, :first_category => 'スポーツ・レジャー'
+  create :id => 10, :first_category => 'ハンドメイド'
+  create :id => 11, :first_category => 'チケット'
+  create :id => 12, :first_category => '自動車・オートバイ'
+  create :id => 13, :first_category => 'その他'
+  create :id => 14, :first_category => 'カテゴリー一覧'
+
+  has_many :categories
+end

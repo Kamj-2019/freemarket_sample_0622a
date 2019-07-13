5b3c920f211f30c0a3642b4625f9a472be25f96f
Hiroto <heeilxh.sev.012@gmail.com>
2019-07-13 15:50:30 +0900

    Active Hash summery

---
+++ app/models/evaluation.rb
@@ -0,0 +1,8 @@
+class Evaluation < ActiveHash::Base
+  field :evaluation
+  create :id => 1, :evaluation => '良い'
+  create :id => 2, :evaluation => '普通'
+  create :id => 3, :evaluation => '悪い'
+
+  has_many :users
+end

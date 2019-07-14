14778ca824b8b23afed83852996bb6d07a9b1603
Queenie <49942420+Queenieaman@users.noreply.github.com>
2019-07-14 19:25:37 +0900

    Create Table (Item)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190707130850) do
+ActiveRecord::Schema.define(version: 20190713085641) do
+
+  create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "item_title",  limit: 65535
+    t.text     "description", limit: 65535
+    t.integer  "price"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "nickname",                            null: false

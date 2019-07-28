8bc6b30ae3d07ccced8a3d1067843132b5d3644e
KamJ-2019 <jnk.tcexp.ac@gmail.com>
2019-07-28 12:12:25 +0900

    Summary

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,17 @@
+class ItemsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+  end
+
+  def show
+    @item = Item.new
+  end
+
+end

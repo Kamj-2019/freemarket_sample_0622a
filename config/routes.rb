790b6a540a6404fff6a71c80d83951a4c291df3c
KamJ-2019 <jnk.tcexp.ac@gmail.com>
2019-08-04 16:07:14 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'items#index'
-  resources :items
-  resources :users do
+  resources :items, only: [:index, :new, :create, :show]
+  resources :users, only: [:index, :show, :edit] do
     collection do
       get 'logout'
     end

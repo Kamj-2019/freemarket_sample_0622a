18879901fc68ec0de93ca34b5873690e880ee86a
momoko-n <mm_ayhrn@yahoo.co.jp>
2019-08-19 15:53:47 +0900

    update step1.html.haml

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,13 @@ Rails.application.routes.draw do
       get 'signup'
     end
   end
+  resources :registration do
+    collection do
+      get 'step1' # 会員情報を入力
+      get 'step2' # 電話番号認証
+      get 'step3' # 住所入力
+      get 'step4' # 支払い方法
+      get 'done' # 完了画面
+    end
+  end
 end

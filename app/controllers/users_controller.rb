e2fe8ec7e6588fa0993490c93616cd340608b583
KamJ-2019 <jnk.tcexp.ac@gmail.com>
2019-08-21 20:31:47 +0900

    Update user_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,29 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = current_user
   end
 
   def edit
+    @user = current_user
+  end
+
+  def update
+    current_user.update(user_params)
+  end
+
+  def profile
+    @user = current_user
+  end
+
+  def card
+    @user = current_user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email)
   end
 
 end

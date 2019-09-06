98e9b64adfa96f9f4372ab862553bf7254737e68
KamJ-2019 <jnk.tcexp.ac@gmail.com>
2019-09-06 23:26:18 +0900

    Update cards_controller.rb

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,9 +2,36 @@ class CardsController < ApplicationController
 
   require "payjp"
 
+  def index
+    @card = Card.where(user_id: current_user.id).first
+
+    if @card.present?
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
+      @card_brand = @default_card_information.brand      
+      case @card_brand
+      when "Visa"
+        @card_src = "visa.svg"
+      when "JCB"
+        @card_src = "jcb.svg"
+      when "MasterCard"
+        @card_src = "master-card.svg"
+      when "American Express"
+        @card_src = "american_express.svg"
+      when "Saison Card"
+        @card_src = "saison-card.svg"
+      when "Diners Club"
+        @card_src = "dinersclub.svg"
+      when "Discover"
+        @card_src = "discover.svg"
+      end
+    end
+  end
+
   def new
     card = Card.where(user_id: current_user.id)
-    redirect_to action: "show" if card.exists?
+    redirect_to action: "index" if card.exists?
   end
 
   def pay 
@@ -14,11 +41,11 @@ class CardsController < ApplicationController
     else
       customer = Payjp::Customer.create(
       description: '登録テスト',
-      card: params['payjp-token'],
+      card: params['payjp-token']
       ) 
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
-        redirect_to action: "show"
+        redirect_to action: "index"
       else
         redirect_to action: "pay"
       end
@@ -34,17 +61,10 @@ class CardsController < ApplicationController
       customer.delete
       card.delete
     end
-      redirect_to action: "new"
+      redirect_to action: "index"
   end
 
   def show
-    card = Card.where(user_id: current_user.id).first
-    if card.blank?
-      redirect_to action: "new" 
-    else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
-    end
+
   end
 end

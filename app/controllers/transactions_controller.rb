37444ab010bbf48b71845dc3e19e14de77a8f420
Queenie <49942420+Queenieaman@users.noreply.github.com>
2019-09-07 16:31:29 +0900

    Update transaction.controller.rb (add set_card)

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,28 +1,26 @@
 class TransactionsController < ApplicationController 
   before_action :set_item
+  before_action :set_card
   require 'payjp'
 
   def show
-    card = Card.where(user_id: current_user.id).first
-    #Cardテーブルからpayjpの顧客IDを検索
-    if card.blank?
+    if @card.blank?
       #登録された情報がない場合にカード登録画面に移動
       redirect_to controller: "card", action: "new"
       flash[:alert] = '購入にはクレジットカード登録が必要です'
     else
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
     #保管した顧客IDでpayjpから情報取得
-    customer = Payjp::Customer.retrieve(card.customer_id)
+    customer = Payjp::Customer.retrieve(@card.customer_id)
     #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
-    @default_card_information = customer.cards.retrieve(card.card_id)
+    @default_card_information = customer.cards.retrieve(@card.card_id)
   end
 
   def pay
-    card = Card.where(user_id: current_user.id).first
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
     amount: @item.price, #支払金額を入力（itemテーブル等に紐づけても良い）
-    customer: card.customer_id, #顧客ID
+    customer: @card.customer_id, #顧客ID
     currency: 'jpy', #日本円
     )
     redirect_to action: 'done' #完了画面に移動
@@ -30,16 +28,20 @@ class TransactionsController < ApplicationController
 end 
 
   def  done
-    card = Card.where(user_id: current_user.id).first
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-    customer = Payjp::Customer.retrieve(card.customer_id)
-    @default_card_information = customer.cards.retrieve(card.card_id)
+    customer = Payjp::Customer.retrieve(@card.customer_id)
+    @default_card_information = customer.cards.retrieve(@card.card_id)
   end
 
   private
 
   def set_item
     @item = Item.find(params[:id])
+  end
+  
+  #Cardテーブルからpayjpの顧客IDを検索
+  def set_card
+    @card = Card.find_by(user_id: current_user.id)
   end
 end
 

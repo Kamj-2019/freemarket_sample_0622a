9c3d9a767724f6ac6dea2a475d43772afee5c647
Queenie <49942420+Queenieaman@users.noreply.github.com>
2019-09-07 16:00:34 +0900

    Update transaction_controller.rb

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -18,21 +18,19 @@ class TransactionsController < ApplicationController
     @default_card_information = customer.cards.retrieve(card.card_id)
   end
 
-    def pay
-      @item = Item.find(params[:id])
-      card = Card.where(user_id: current_user.id).first
-      Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
-      Payjp::Charge.create(
-      amount: @item.price, #支払金額を入力（itemテーブル等に紐づけても良い）
-      customer: card.customer_id, #顧客ID
-      currency: 'jpy', #日本円
-      )
-      redirect_to action: 'done' #完了画面に移動
-    end
-  end 
+  def pay
+    card = Card.where(user_id: current_user.id).first
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    Payjp::Charge.create(
+    amount: @item.price, #支払金額を入力（itemテーブル等に紐づけても良い）
+    customer: card.customer_id, #顧客ID
+    currency: 'jpy', #日本円
+    )
+    redirect_to action: 'done' #完了画面に移動
+  end
+end 
 
   def  done
-    @item = Item.find(params[:id])
     @user = current_user
     card = Card.where(user_id: current_user.id).first
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]

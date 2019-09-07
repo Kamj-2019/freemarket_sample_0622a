0c5f4f4dabacb68059f200681fc8dbfd7afd306a
Queenie <49942420+Queenieaman@users.noreply.github.com>
2019-09-07 15:06:30 +0900

    Update transaction_controller.rb
    show, pay, done including payjp

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,7 +1,47 @@
-class TransactionsController < ApplicationController
+class TransactionsController < ApplicationController 
 
-  def edit
+  require 'payjp'
+
+  def show
     @item = Item.find(params[:id])
+    @user = current_user
+    card = Card.where(user_id: current_user.id).first
+    #Cardテーブルからpayjpの顧客IDを検索
+    if card.blank?
+      #登録された情報がない場合にカード登録画面に移動
+      redirect_to controller: "card", action: "new"
+      flash[:alert] = '購入にはクレジットカード登録が必要です'
+    else
+    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    #保管した顧客IDでpayjpから情報取得
+    customer = Payjp::Customer.retrieve(card.customer_id)
+    #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
+    @default_card_information = customer.cards.retrieve(card.card_id)
+  end
+
+    def pay
+      @item = Item.find(params[:id])
+      @user = current_user
+      card = Card.where(user_id: current_user.id).first
+      Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+      Payjp::Charge.create(
+      amount: @item.price, #支払金額を入力（itemテーブル等に紐づけても良い）
+      customer: card.customer_id, #顧客ID
+      currency: 'jpy', #日本円
+      )
+      redirect_to action: 'done' #完了画面に移動
+    end
+  end 
+
+  def  done
+    @item = Item.find(params[:id])
+    @user = current_user
+    card = Card.where(user_id: current_user.id).first
+    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    customer = Payjp::Customer.retrieve(card.customer_id)
+    @default_card_information = customer.cards.retrieve(card.card_id)
   end
 
 end
+
+

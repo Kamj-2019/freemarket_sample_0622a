87a8787f81e5d1e51169964cfc20cc016300dee9
KamJ-2019 <jnk.tcexp.ac@gmail.com>
2019-09-11 22:06:52 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
+  def production?
+    Rails.env.production?
+  end
+  
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, user_detail_attributes:[:first_name, :family_name, :kana_first_name, :kana_family_name, :birthday, :phone_number,  :postalcode, :prefecture_id, :city, :address, :building]])

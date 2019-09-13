3750a8150eacf760763adcfcc17c4090c268f155
Queenie <49942420+Queenieaman@users.noreply.github.com>
2019-09-13 21:27:03 +0900

    Comment item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,22 +1,28 @@
 class Item < ApplicationRecord
   include AASM
 
-  aasm do
+  aasm :column => 'aasm_state' do
     # 状態の定義
-    state :waiting, :initial => true
+    state :waiting, :initial => true #デフォルト値の指定
     state :wip, :done
+
+    # waiting-出品中
+    # wip-取引中
+    # done-売却済
 
     # イベントの定義
     event :run do
-      # 遷移の定義
+      # 遷移の定義（出品中→取引中）
       transitions :from => :waiting, :to => :wip
     end
 
     event :finish do
+      # 遷移の定義（取引中→売却済）
       transitions :from => :wip, :to => :done
     end
 
     event :wait do
+      # 遷移の定義（キャンセル：→出品中への戻し）
       transitions :from => [:wip, :done], :to => :waiting
     end
   end
